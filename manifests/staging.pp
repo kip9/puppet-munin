c5ece94f03af6b76c20eeba94f02428305d93474
Jakub Jakubiec <jakubiec@viddler.com>
2013-07-26 20:17:21 +0200

    Added staging node manifest initial version

---
+++ manifests/staging.pp
@@ -0,0 +1,14 @@
+# Install PHP CLI
+include php::cli
+
+# Install mcrypt and mysql modules for php
+php::module { [ 'mcrypt', 'mysql' ]: }
+
+# Install FPM
+include php::fpm::daemon
+php::fpm::conf { 'www':
+  listen  => '127.0.0.1:9001',
+  user    => 'nginx',
+  # For the user to exist
+  require => Package['nginx'],
+}
