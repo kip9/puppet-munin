e7bff3023968114648abd6c5b9a25f137b710698
Jakub Jakubiec <jakubiec@viddler.com>
2013-09-30 18:49:40 +0200

    Commented out DB configuration file, which is not handled well outside of vagrant

--- modules/barley/manifests/app.pp
+++ modules/barley/manifests/app.pp
@@ -128,14 +128,14 @@ class barley::app ($environment = 'staging') {
   }
 
   # App DB config
-  file { '/var/www/barley.plain/application/config/development/database.php':
-    ensure  => 'present',
-    owner   => "${nginx::params::nx_daemon_user}",
-    mode    => 0600,
-    source  => "puppet:///modules/barley/application/config/development/database.php",
-    notify  => Exec['barley_db_migrate'],
-    require => Database['barley'],
-  }
+ # file { '/var/www/barley.plain/application/config/development/database.php':
+ #   ensure  => 'present',
+ #   owner   => "${nginx::params::nx_daemon_user}",
+ #   mode    => 0600,
+ #   source  => "puppet:///modules/barley/application/config/development/database.php",
+ #   notify  => Exec['barley_db_migrate'],
+ #   require => [ Database['barley'], ],
+ # }
 
   exec { 'barley_db_migrate':
     cwd         => "${barley::params::app_dir}",
